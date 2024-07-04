24a6fde2824bd2a9ef4703528bbb835855c09bc4
Sh1rkhan <huseynov.shirkhan@gmail.com>
2024-07-05 02:27:01 +0400

    add js-task 11 dom p II

---
+++ js-task 11 dom p II/app.js
@@ -0,0 +1,42 @@
+// TASK I method 1
+
+// const body = document.body
+
+// let gray = document.querySelector(".gray")
+// let red = document.querySelector(".red")
+// let blue = document.querySelector(".blue")
+// let yellow = document.querySelector(".yellow")
+
+
+// gray.onclick = () => {
+//     body.style.backgroundColor = "gray"
+//  }
+//  red.onclick = () => {
+//     body.style.backgroundColor = "red"
+//  }
+//  blue.onclick = () => {
+//     body.style.backgroundColor = "blue"
+//  }
+//  yellow.onclick = () => {
+//     body.style.backgroundColor = "yellow"
+//  }
+ 
+
+
+// TASK I method 2 -!!!ATTENTION!!! 
+// // for this method we need to turn off styles of circles in index.html (lines: 37-40)
+
+// const body = document.body
+// let circles = document.querySelectorAll(".c1")
+
+// let gray = document.querySelector(".gray").style.backgroundColor = "gray"
+// let red = document.querySelector(".red").style.backgroundColor = "red"
+// let blue = document.querySelector(".blue").style.backgroundColor = "blue"
+// let yellow = document.querySelector(".yellow").style.backgroundColor = "yellow"
+
+
+// circles.forEach(element => {
+//     element.onclick  = () => {
+//         body.style.backgroundColor =  element.style.backgroundColor
+//     }})
+
