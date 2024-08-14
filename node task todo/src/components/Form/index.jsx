6513ecc1fa6dab7e5d1bab4fbf889bfed4427976
Sh1rkhan <huseynov.shirkhan@gmail.com>
2024-08-14 18:05:47 +0400

    change some troubles

--- node task todo/src/components/Form/index.jsx
+++ node task todo/src/components/Form/index.jsx
@@ -1,40 +1,91 @@
-import { useState } from "react";
+import { useState } from 'react';
 import "./style.css"
 
 
 
 const TodoForm = () => {
-  const [todoEvent, setTodo] = useState("")
+
+const [todos, setTodos] = useState([])
+const [input, setInput] = useState("")
+
+const addTodo = () => {
+  if (input.trim()) {
+    setTodos([...todos, input]);
+    setInput('');
+  }
+};
+
+const handleInputChange = (event) => {
+  setInput(event.target.value);
+};
+
+const handleKeyPress = (event) => {
+  if (event.key === 'Enter') {
+    addTodo();
+  }
+};
+
+const handleDelete = (index) => {
+  setTodos(todos.filter((_, i) => i !== index));
+};
+
+
+
+
+  // const [todos, setTodo] = useState("")
 
 
   return (
-    <form onSubmit={(event)=> {
+    // <form onSubmit={(event)=> {
+    //   event.preventDefault()
+    //   todos
+         
+      
+      
+    // }}>
+    //     <h2>Create your new task</h2>
+    //     <div className='add-box'>
+    //     <input type="text" placeholder='enter Todo here...'
+    //       onChange={(event) => {
+    //         setTodo(event.target.value)
+    //         // console.log(todoEvent);
+            
+    //       }}/>
+    //     <button>Add new todo</button>    
+    //     </div>
+          
+    //     <div className="todoList">
+    //       <p>{todo}</p>
+    //     </div>
+    // </form>
+
+<form onSubmit={(event)=> {
       event.preventDefault()
     
-
-   
+         
       
       
     }}>
-        <h2>Create your new task</h2>
-        <div className='add-box'>
-        <input type="text" placeholder='enter Todo here...'
-          onChange={(event) => {
-            setTodo(event.target.value)
-            console.log(todoEvent);
-            
-          }}/>
-        <button>Add new todo</button>    
-        </div>
-          
-        <div>
-          <p>{todoEvent}</p>
-        </div>
-    </form>
-
+    <input
+    type="text"
+    value={input}
+    onChange={handleInputChange}
+    onKeyPress={handleKeyPress}
+    placeholder="Add a new todo"
+  />
+  <button onClick={addTodo}>Add</button>
+  <ul>
+    {todos.map((todo, index) => (
+      <li key={index}>
+        {todo}
+        <button onClick={() => handleDelete(index)}>Delete</button>
+      </li>
+    ))}
+  </ul>
+  </form>
     
 
   )
 }
 
-export default TodoForm
+export default TodoForm 
